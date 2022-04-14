cdeca697be256cd4b038d7ad6d52a92b20a73a5f
ApolloDionysian <j.ahmad@alhisba.com>
2022-04-14 04:05:14 +0300

    add plan

--- src/composer/Main.ts
+++ src/composer/Main.ts
@@ -6,21 +6,29 @@ import { Observable } from 'rxjs';
 let randomName = faker.name.findName(); // Rowan Nikolaus
 let randomEmail = faker.internet.email();
 
-
+// TODO: init a secure role based Parse server & dashboard & use it to Implement Generic Actions, Effects, Reducers & selectors
+// TODO: Decide with the guys if we should use firebase instead since its faster to implement
 // TODO: Create Generic Actions: 
 /**
- * read: (Multiple & Single. Observable & Promise. Local or remote(Try, success, fail))
- * write: (Create, Update, & Delete. Multiple & single. Local or remote(Try, success, fail))) all async
+ * class Action<T>{
+ * read: (Multiple||Single, Observable||Promise, Local||remote) => (1 action if local || 3 if remote , effects if remote, reducers, Selectors)
+ * write: (Create||Update||Delete, Multiple||single, Local || remote) => (1 action if local || 3 actions if remote, effects if remote, reducers if local)
  */
 //TODO: Create Generic Component
 /**
  * props: {...local, ...remote} => create local & remote crud actions
  * parent: in dom tree
- * views: a tree of {element, {slot,}}
- * 
+ * views: {element, {slot,prop}[], {event, action}, views: []} Tree
+ * lifeCycleEvents: {lifecycle, readactions}[]
+ */
+// TODO: implement override operations for generic component 
+/**
+ * addition : a.props + b.props  = c.props (new entity) 
+ * subtraction : a.props - b.props  = c.props (new entity)
+ * prioritize : dependencyTree.priotize((a,b) => a > b) => b.validations = b.validations + a.validations
+ * compose: compose((a,b, slot) => a > b) => a.views.slot + new selection view of b. 
  * 
  */
-// TODO: impolement Composition 
 interface Action<T>{
     name: string;
     params: {[i:string]: T}
